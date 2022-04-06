0ac167540cd53cab263ed400934b64ca56e92c58
Fernando Galvan <n.brighter00@gmail.com>
2022-04-06 12:52:09 -0500

    added functionailty and if else statement

--- express-lab-2/pass-it-around/server.js
+++ express-lab-2/pass-it-around/server.js
@@ -6,7 +6,6 @@ const port = 3000;
 
 
 app.get("/", (req, res) => {
-    // res.send("99 Bottles of Beer On The Wall")
     res.send(`
     <html>
         <body>
@@ -16,19 +15,25 @@ app.get("/", (req, res) => {
     </htmL>
     `)
 })
-
-
-app.get("/:number", (req,res) =>{
-    if(req.params.number === "0") {
+app.get("/:number", (req,res) => {
+    if(req.params.number === "1") {
         res.send(`
     <html>
         <body>
+        <p> There are no more bottles left </P>
         <a href = "/">Back to home</a>
         </body>    
     </html>`
     )}
+    else {
+        res.send(req.params.number - 1 +' Bottles of beer left on the wall')
+    }
 })
 
+// //If user inputs number, send back text saying number of bottles left and url
+// app.get("/:numberofbottles/", (req, res) => {
+// res.send(req.params.numberofbottles - 1 +' Bottles of beer left on the wall')
+// })
 
 app.listen(port, () => {
     console.log('we are on port 3000')
